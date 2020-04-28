eceb8c2a68dfc1945b488022729a2b88920c7769
Shehana Iqbal <iqbalshehana@gmail.com>
2020-04-28 15:35:39 +0530

    detection model

---
+++ custom/object_detection_yolo.py
@@ -0,0 +1,122 @@
+import cv2 as cv
+import argparse
+import sys
+import numpy as np
+import os.path
+import Demo
+# Initialize the parameters
+confThreshold = 0.5  #Confidence threshold
+nmsThreshold = 0.4  #Non-maximum suppression threshold
+
+inpWidth = 416  #608     #Width of network's input image
+inpHeight = 416 #608     #Height of network's input image
+
+parser = argparse.ArgumentParser(description='Object Detection using YOLO in OPENCV')
+parser.add_argument('--image', help='Path to image file.')
+parser.add_argument('--video', help='Path to video file.')
+args = parser.parse_args()
+
+# Load names of classes
+classesFile = "classes.names";
+
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+modelConfiguration = "darknet-yolov3.cfg";
+modelWeights = "lapi.weights";
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+def getOutputsNames(net):
+    # Get the names of all the layers in the network
+    layersNames = net.getLayerNames()
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# Draw the predicted bounding box
+def drawPred(classId, conf, left, top, right, bottom):
+    # Draw a bounding box.
+    #    cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
+    cv.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
+
+    label = '%.2f' % conf
+
+    # Get the label for the class name and its confidence
+    if classes:
+        assert(classId < len(classes))
+        label = '%s:%s' % (classes[classId], label)
+        img = cv.imread(outputFile)
+        crop_img = img[top:bottom, left:right]
+        cv.imwrite('cropped.jpg',crop_img)
+        Demo.extract_text()
+# Remove the bounding boxes with low confidence using non-maxima suppression
+def postprocess(frame, outs):
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+    classIds = []
+    confidences = []
+    boxes = []
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        print("out.shape : ", out.shape)
+        for detection in out:
+            #if detection[4]>0.001:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            #if scores[classId]>confThreshold:
+            confidence = scores[classId]
+            if detection[4]>confThreshold:
+                print(detection[4], " - ", scores[classId], " - th : ", confThreshold)
+                print(detection)
+            if confidence > confThreshold:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
+
+winName = 'Deep learning object detection in OpenCV'
+cv.namedWindow(winName, cv.WINDOW_NORMAL)
+
+outputFile = "yolo_out_py.avi"
+if not os.path.isfile(args.image):
+    print("Input image file ", args.image, " doesn't exist")
+    sys.exit(1)
+cap = cv.VideoCapture(args.image)
+outputFile = args.image[:-4]+'_yolo_out_py.jpg'
+
+while cv.waitKey(1) < 0:
+
+    hasFrame, frame = cap.read()
+    if not hasFrame:
+        print("Done processing !!!")
+        print("Output file is stored as ", outputFile)
+        cv.waitKey(3000)
+        break
+    blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+    net.setInput(blob)
+    outs = net.forward(getOutputsNames(net))
+    postprocess(frame, outs)
+    t, _ = net.getPerfProfile()
+    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+    if (args.image):
+        cv.imwrite(outputFile, frame.astype(np.uint8));
